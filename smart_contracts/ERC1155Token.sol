7448d5b55f8c6e210028adcb2ce76e8405c4c4e6
bujosa <davidbujosa@gmail.com>
2023-07-03 03:14:03 -0400

    feat: add ERC1155Token v2

--- smart_contracts/ERC1155Token.sol
+++ smart_contracts/ERC1155Token.sol
@@ -1,17 +1,163 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.1.0/contracts/token/ERC1155/ERC1155.sol";
+contract ERC1155Token {
+    mapping(address => mapping(uint256 => uint256)) public balances;
+    mapping(uint256 => mapping(address => uint256)) public allowances;
+    mapping(address => bool) public operators;
+    uint256 public totalSupply;
 
-contract ERC1155Token is ERC1155 {
-    constructor(string memory _uri) ERC1155(_uri) {}
-
-    function mint(
-        address account,
+    event TransferSingle(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
         uint256 id,
-        uint256 amount,
-        bytes memory data
+        uint256 value
+    );
+    event TransferBatch(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256[] ids,
+        uint256[] values
+    );
+    event ApprovalForAll(
+        address indexed owner,
+        address indexed operator,
+        bool approved
+    );
+    event URI(string value, uint256 indexed id);
+
+    function balanceOf(
+        address _owner,
+        uint256 _id
+    ) public view returns (uint256) {
+        return balances[_owner][_id];
+    }
+
+    function balanceOfBatch(
+        address[] memory _owners,
+        uint256[] memory _ids
+    ) public view returns (uint256[] memory) {
+        uint256[] memory batchBalances = new uint256[](_owners.length);
+        for (uint256 i = 0; i < _owners.length; i++) {
+            batchBalances[i] = balances[_owners[i]][_ids[i]];
+        }
+        return batchBalances;
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) public {
+        operators[_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
+    }
+
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public view returns (bool) {
+        return operators[_operator] || _owner == _operator;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id,
+        uint256 _value
     ) public {
-        _mint(account, id, amount, data);
+        require(_to != address(0), "Invalid recipient");
+        require(_value <= balances[_from][_id], "Insufficient balance");
+
+        balances[_from][_id] -= _value;
+        balances[_to][_id] += _value;
+        emit TransferSingle(msg.sender, _from, _to, _id, _value);
+    }
+
+    function transferBatch(
+        address _from,
+        address _to,
+        uint256[] memory _ids,
+        uint256[] memory _values
+    ) public {
+        require(_to != address(0), "Invalid recipient");
+        require(_ids.length == _values.length, "Arrays length mismatch");
+
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+            require(value <= balances[_from][id], "Insufficient balance");
+
+            balances[_from][id] -= value;
+            balances[_to][id] += value;
+        }
+        emit TransferBatch(msg.sender, _from, _to, _ids, _values);
+    }
+
+    function mint(address _to, uint256 _id, uint256 _value) public {
+        require(_to != address(0), "Invalid recipient");
+
+        balances[_to][_id] += _value;
+        totalSupply += _value;
+        emit TransferSingle(msg.sender, address(0), _to, _id, _value);
+    }
+
+    function mintBatch(
+        address _to,
+        uint256[] memory _ids,
+        uint256[] memory _values
+    ) public {
+        require(_to != address(0), "Invalid recipient");
+        require(_ids.length == _values.length, "Arrays length mismatch");
+
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+            balances[_to][id] += value;
+            totalSupply += value;
+        }
+        emit TransferBatch(msg.sender, address(0), _to, _ids, _values);
+    }
+
+    function burn(address _from, uint256 _id, uint256 _value) public {
+        require(_from != address(0), "Invalid owner");
+        require(_value <= balances[_from][_id], "Insufficient balance");
+
+        balances[_from][_id] -= _value;
+        totalSupply -= _value;
+        emit TransferSingle(msg.sender, _from, address(0), _id, _value);
+    }
+
+    function burnBatch(
+        address _from,
+        uint256[] memory _ids,
+        uint256[] memory _values
+    ) public {
+        require(_from != address(0), "Invalid owner");
+        require(_ids.length == _values.length, "Arrays length mismatch");
+
+        for (uint256 i = 0; i < _ids.length; i++) {
+            uint256 id = _ids[i];
+            uint256 value = _values[i];
+            require(value <= balances[_from][id], "Insufficient balance");
+
+            balances[_from][id] -= value;
+            totalSupply -= value;
+        }
+        emit TransferBatch(msg.sender, _from, address(0), _ids, _values);
     }
 }
+
+/*
+This simple example of an ERC1155 contract includes the characteristic methods of an ERC1155 contract:
+
+balanceOf: Returns the token balance of a specific owner for a given token ID.
+balanceOfBatch: Returns the token balances of multiple owners for an array of token IDs.
+setApprovalForAll: Sets or revokes the permission of an operator to spend tokens on behalf of an owner.
+isApprovedForAll: Checks if an operator is approved to spend tokens on behalf of an owner.
+transferFrom: Transfers a specific value of tokens from one owner to another.
+transferBatch: Transfers multiple values of tokens from one owner to another.
+mint: Creates new tokens and assigns them to a recipient.
+mintBatch: Creates multiple tokens and assigns them to a recipient.
+burn: Removes a specific value of tokens from an owner's balance.
+burnBatch: Removes multiple values of tokens from an owner's balance.
+These methods allow for the management of token balances, transfers, token creation and removal, and the configuration of permissions for operators. Please note that this is a basic example, and additional functionality and logic can be added based on the specific needs of the contract and project requirements.
+*/
