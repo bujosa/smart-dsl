ce9cdec6b0fb8f16803d5b2049d5689aba15a87f
bujosa <davidbujosa@gmail.com>
2023-08-21 21:17:01 -0400

    feat: finish doctor bill

--- examples/src-gen/DoctorBill.go
+++ examples/src-gen/DoctorBill.go
@@ -8,9 +8,51 @@ import (
 
 type DoctorBill struct {
 	contractapi.Contract
+	insurer string
+	doctor string
+	patient string
+	amount uint64
+}
+
+func (sc *DoctorBill) GetInsurer(ctx contractapi.TransactionContextInterface) (string, error) {
+	return sc.insurer, nil
+}
+
+func (sc *DoctorBill) GetDoctor(ctx contractapi.TransactionContextInterface) (string, error) {
+	return sc.doctor, nil
+}
+
+func (sc *DoctorBill) GetPatient(ctx contractapi.TransactionContextInterface) (string, error) {
+	return sc.patient, nil
+}
+
+func (sc *DoctorBill) GetAmount(ctx contractapi.TransactionContextInterface) (uint64, error) {
+	return sc.amount, nil
+}
+
+func (sc *DoctorBill) CreateInvoice(ctx contractapi.TransactionContextInterface) error {
+	// This method is for create the invoice
+	if ctx.GetClientIdentity().GetID()==sc.doctor {
+		return fmt.Errorf("Only Doctor")
+	}
+
+	return nil
+}
+
+func (sc *DoctorBill) PayInvoice(ctx contractapi.TransactionContextInterface) error {
+	// This method is for pay the invoice
+	if ctx.GetClientIdentity().GetID()==sc.insurer {
+		return fmt.Errorf("Only Insurer")
+	}
+
+	return nil
 }
 
 func (sc *DoctorBill) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	sc.insurer = ""
+	sc.doctor = ""
+	sc.patient = ""
+	sc.amount = 0
 	return nil
 }
 
--- examples/src-gen/DoctorBill.sol
+++ examples/src-gen/DoctorBill.sol
@@ -2,7 +2,50 @@
 pragma solidity ^0.8.0;
 
 contract DoctorBill {
-	constructor() {
+	address insurer;
+	address doctor;
+	string patient;
+	uint256 amount;
+
+	modifier onlyDoctor() {
+		require(msg.sender == doctor, "Only Doctor");
+		_;
+	}
+
+	modifier onlyInsurer() {
+		require(msg.sender == insurer, "Only Insurer");
+		_;
+	}
+
+	constructor(address _insurer, address _doctor, string memory _patient, uint256 _amount) {
+		insurer = _insurer;
+		doctor = _doctor;
+		patient = _patient;
+		amount = _amount;
+	}
+
+	function getInsurer() public view returns (address) {
+		return insurer;
+	}
+
+	function getDoctor() public view returns (address) {
+		return doctor;
+	}
+
+	function getPatient() public view returns (string memory) {
+		return patient;
+	}
+
+	function getAmount() public view returns (uint256) {
+		return amount;
+	}
+
+	function CreateInvoice() public onlyDoctor  {
+		// This method is for create the invoice
+	}
+
+	function PayInvoice() public onlyInsurer  {
+		// This method is for pay the invoice
 	}
 
 }
